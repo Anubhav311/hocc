0789a29d56bab77d38be2af96c95d60d598a18f0
Anubhav311 <anubhavbhambri1@gmail.com>
2019-12-03 23:39:16 +0530

    improved solution

--- telephone_words.py
+++ telephone_words.py
@@ -34,56 +34,82 @@ digits_to_letters = {
     9: 'WXYZ'
 }
 
+# BRUTE FORCE SOLUTION
 # we will assume only 2 digits
 # 27
+# def telephone_words(digits):
+#     # store a list of perms for the result
+#     perms = []
+#     # check length of the digits
+#     # if the lenght is 1 return single letter Perm
+#     if len(digits) == 1:
+#         # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
+#         for l in digits_to_letters[int(digits[0])]:
+#             perms.append(l)
+
+#     # else if len is 2 
+#     elif len(digits) == 2: 
+#         # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
+#         for l in digits_to_letters[int(digits[0])]:
+#             perms.append(l)
+#             # loop over the letters of the second digit
+#             for m in digits_to_letters[int(digits[1])]:
+#                 # concatonate the letters together and add them to the perms list
+#                 perms.append(l + m)
+#     elif len(digits) == 3: 
+#         # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
+#         for l in digits_to_letters[int(digits[0])]:
+#             perms.append(l)
+#             # loop over the letters of the second digit
+#             for m in digits_to_letters[int(digits[1])]:
+#                 # concatonate the letters together and add them to the perms list
+#                 perms.append(l + m)
+#                 for n in digits_to_letters[int(digits[2])]:
+#                     # concatonate the letters together and add them to the perms list
+#                     perms.append(l + m + n)
+#     elif len(digits) == 4: 
+#         # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
+#         for l in digits_to_letters[int(digits[0])]:
+#             perms.append(l)
+#             # loop over the letters of the second digit
+#             for m in digits_to_letters[int(digits[1])]:
+#                 # concatonate the letters together and add them to the perms list
+#                 perms.append(l + m)
+#                 for n in digits_to_letters[int(digits[2])]:
+#                     # concatonate the letters together and add them to the perms list
+#                     perms.append(l + m + n)
+#                     for o in digits_to_letters[int(digits[3])]:
+#                         # concatonate the letters together and add them to the perms list
+#                         perms.append(l + m + n + o)
+
+#     # return perms list
+#     return perms
+
+
+# return perms list
+#   return perms
+
+# helper function
+def helper_function(words_so_far, letters):
+    # create a new list of words
+    new_words = []
+    # iterate ofer each word in the words so far list
+    for word in words_so_far:
+        # iterate over each letter in the letters list
+        for letter in letters:
+            # append the word and letter to the new words
+            new_words.append(word + letter)
+    # return new words
+    return new_words
+
 def telephone_words(digits):
-    # store a list of perms for the result
-    perms = []
-    # check length of the digits
-    # if the lenght is 1 return single letter Perm
-    if len(digits) == 1:
-        # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
-        for l in digits_to_letters[int(digits[0])]:
-            perms.append(l)
-
-    # else if len is 2 
-    elif len(digits) == 2: 
-        # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
-        for l in digits_to_letters[int(digits[0])]:
-            perms.append(l)
-            # loop over the letters of the second digit
-            for m in digits_to_letters[int(digits[1])]:
-                # concatonate the letters together and add them to the perms list
-                perms.append(l + m)
-    elif len(digits) == 3: 
-        # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
-        for l in digits_to_letters[int(digits[0])]:
-            perms.append(l)
-            # loop over the letters of the second digit
-            for m in digits_to_letters[int(digits[1])]:
-                # concatonate the letters together and add them to the perms list
-                perms.append(l + m)
-                for n in digits_to_letters[int(digits[2])]:
-                    # concatonate the letters together and add them to the perms list
-                    perms.append(l + m + n)
-    elif len(digits) == 4: 
-        # loop over letters in the digits to letters dictionary using the digit at 0th position as the KeyboardInterrupt
-        for l in digits_to_letters[int(digits[0])]:
-            perms.append(l)
-            # loop over the letters of the second digit
-            for m in digits_to_letters[int(digits[1])]:
-                # concatonate the letters together and add them to the perms list
-                perms.append(l + m)
-                for n in digits_to_letters[int(digits[2])]:
-                    # concatonate the letters together and add them to the perms list
-                    perms.append(l + m + n)
-                    for o in digits_to_letters[int(digits[3])]:
-                        # concatonate the letters together and add them to the perms list
-                        perms.append(l + m + n + o)
-
-    # return perms list
+    # perms list
+    perms = ['']
+    # iterate over the digits
+    for digit in digits:
+        # set perms the return value of our helper function using perms and the deigits to letters at the index of digit
+        perms = helper_function(perms, digits_to_letters[int(digit)])
+    # return perms
     return perms
 
-
-
-print(telephone_words('2347'))
+print(telephone_words('27'))
