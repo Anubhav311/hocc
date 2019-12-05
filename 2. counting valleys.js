4a2bc038cd896add61d248c87b8d74f24f850ddb
Anubhav311 <anubhavbhambri1@gmail.com>
2019-12-05 22:28:40 +0530

    solution to counting valleys problem

--- 2. counting valleys.js
+++ 2. counting valleys.js
@@ -1,4 +1,4 @@
-'''
+/*
 Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. During his last hike he took exactly  steps. For every step he took, he noted if it was an uphill, , or a downhill,  step. Gary's hikes start and end at sea level and each step up or down represents a  unit change in altitude. We define the following terms:
 
 A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
@@ -43,4 +43,26 @@ _/\      _
    \    /
     \/\/
 He enters and leaves one valley.
-'''
+*/
+
+function countingValleys(n, s) {
+    let currentLevel = 0
+    let numberOfValleys = 0
+    const steps = []
+    // Make string into array
+    for (const step in s) {
+        steps.push(s[step])
+    }
+    steps.forEach((step, idx) => {
+        if (step == "D") {
+            currentLevel -= 1
+            // if currentLevel is -1, set currentlyInValley to true & add 1 to N
+            if (currentLevel == -1) {
+                numberOfValleys += 1
+            }
+        } else {
+            currentLevel += 1
+        }
+    })
+    return numberOfValleys
+}
